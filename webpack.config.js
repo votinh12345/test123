40807dfe6f095f8c4f1e72650554e348f9a6dd8e
hiennv6925 <hiennv6925@co-well.com.vn>
2018-12-28 10:03:12 +0700

    common_v2.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
     entry: resolve(__dirname, 'index.js'),
     output: {
-        path: resolve(__dirname, 'test11'),
-        filename: 'test1.js',
+        path: resolve(__dirname, 'hiennv2.0.0'),
+        filename: 'v2.0.0_test.js',
         library: 'index'
     },
     plugins: [
